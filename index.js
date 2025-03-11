05a967ba5a013bf9600e4e8f8249906aa46133fb
BryOk-droid <brianokoth534@gmail.com>
2025-03-11 16:36:35 +0300

    Complete findMatching, fuzzyMatch, and matchName functions

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+// Function: findMatching
+function findMatching(drivers, name) {
+  return drivers.filter(
+    (driver) => driver.toLowerCase() === name.toLowerCase()
+  );
+}
+
+// Function: fuzzyMatch
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter((driver) => driver.startsWith(letters));
+}
+
+// Function: matchName
+function matchName(drivers, name) {
+  return drivers.filter((driver) => driver.name === name);
+}
